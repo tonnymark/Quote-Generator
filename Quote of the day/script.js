4f1bb6bdb80f21bd6b124291a6c780de27eea41f
tonnymark <119018688+tonnymark@users.noreply.github.com>
2023-12-02 22:52:21 -0700

    Add files via upload

--- Quote of the day/script.js
+++ Quote of the day/script.js
@@ -2,18 +2,21 @@ const quote_Api = 'https://api.quotable.io/random';
 
 const generateQuote = document.getElementById('generate_quote');
 generateQuote.addEventListener('click', fetchQuote);
-
 const autor_name = document.querySelector('.author_name');
-
 const quoteArea = document.getElementById('quote-holder');
 
 async function fetchQuote() {
   try {
+    quoteArea.innerText = 'Loading...';
+    generateQuote.innerText = 'Loading...';
     const response = await fetch(quote_Api);
     const data = await response.json();
-    quoteArea.innerHTML = data.content;
-    autor_name.innerText = '... ' + data.author;
+    quoteArea.innerText = data.content;
+    autor_name.innerText = '~  ' + data.author;
+    generateQuote.innerText = 'Get new quote';
   } catch (error) {
+    quoteArea.innerText = 'Something goes wrong. Try again !';
+    autor_name.innerText = '~ not available ';
     console.log(error);
   }
 }
